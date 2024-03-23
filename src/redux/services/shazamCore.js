ab1d825de3720cb93955a1e2c5a38791fc69faee
Viren-45 <2000viren@gmail.com>
2024-03-23 08:39:56 -0400

    Initial commit

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,28 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+    export const shazamCoreApi = createApi({
+        reducerPath: 'shazamCoreApi',
+        baseQuery: fetchBaseQuery({
+            baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+            prepareHeaders: (headers) => {
+                headers.set('X-RapidAPI-Key', 'd567bbac13msh0fbab99b37e545dp1cd387jsn71e6d0e08ba9');
+
+                return headers;
+            }
+        }),
+        endpoints: (builder) => ({
+            getTopCharts: builder.query({ query: () => '/charts/world' }),
+            getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
+            getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
+            getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
+            getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
+        }),
+    });
+
+    export const {
+        useGetTopChartsQuery,
+        useGetSongDetailsQuery,
+        useGetSongRelatedQuery,
+        useGetArtistDetailsQuery,
+        useGetSongsByCountryQuery
+    } = shazamCoreApi;
